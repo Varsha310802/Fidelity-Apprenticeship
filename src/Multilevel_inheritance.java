4579c2c92bc5b02ae01ea989a65b4eb71fce2c36
Varsha310802 <132434004+Varsha310802@users.noreply.github.com>
2024-09-30 17:10:07 +0530

    Delete src directory

--- src/Multilevel_inheritance.java
+++
@@ -1,9 +0,0 @@
-public class Multilevel_inheritance {
-        public static void main(String[] args){
-            Dog dog = new Dog();
-            dog.eat();
-            dog.walk();
-            dog.bark();
-        }
-    }
-
