1f12f589efdf58893b2a12c51e56a73b7d003ac5
Varsha310802 <132434004+Varsha310802@users.noreply.github.com>
2024-09-30 07:56:31 -0400

    Assessment 2

---
+++ Assesment 2/src/UserDefinedException.java
@@ -0,0 +1,20 @@
+ class UserDefinedException extends Exception {
+    public UserDefinedException(String message) {
+        super(message);
+    }
+}
+class Employee{
+    private int id;
+    private String name;
+
+    public Employee(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+    public int getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+}
